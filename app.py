04ad0a40626e515343be1fb972694b6e026302e6
Florian Klampfer <f.klampfer@gmail.com>
2013-06-25 23:57:35 +0200

    upgrade to python 2.7 and webapp2

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
 import os
+import webapp2
 
 from google.appengine.ext import db
-from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
-from google.appengine.ext.webapp.util import run_wsgi_app
 
-class MainPage(webapp.RequestHandler):
+class MainPage(webapp2.RequestHandler):
 	def get(self, file):
-		template_values = {
-			'title': 'Stringer Bell',
-		}
-								
-		path = os.path.join(os.path.dirname(__file__), 'index.html')
-		self.response.out.write(template.render(path, template_values))
+	  template_values = {
+	  	'title': 'Stringer Bell',
+	  }
+	  						
+	  path = os.path.join(os.path.dirname(__file__), 'index.html')
+	  self.response.out.write(template.render(path, template_values))
 
-application = webapp.WSGIApplication(
-	[(r'/(.*)', MainPage)
-	],debug=False)
-									 
-def main():
-	run_wsgi_app(application)
-
-if __name__ == "__main__":
-	main()
-
+app = webapp2.WSGIApplication([(r'/(.*)', MainPage)])
